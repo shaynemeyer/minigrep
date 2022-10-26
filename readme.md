f0199de1fc8850cd78aee1fceda342cde42e8fcb
Shayne Meyer <shayne.meyer@gmail.com>
2022-10-26 07:59:13 -0700

    finished initial program

---
+++ readme.md
@@ -0,0 +1,3 @@
+# minigrep
+
+This is a command line tool that trys to do what the grep command does. It is solely intented for me to work through while mastering Rust Language concepts.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use minigrep::Config;
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
