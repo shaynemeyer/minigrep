a98951f51515eff72ed1db179129abd36b98209b
Shayne Meyer <shayne.meyer@gmail.com>
2022-10-28 06:41:29 -0700

    upgraded to use iterators and updated the search methods

--- src/lib.rs
+++ src/lib.rs
@@ -62,15 +62,6 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
         .lines()
         .filter(|line| line.to_lowercase().contains(&query))
         .collect()
-
-    // let mut results = Vec::new();
-    // for line in contents.lines() {
-    //     if line.to_lowercase().contains(&query) {
-    //         results.push(line);
-    //     }
-    // }
-
-    // results
 }
 
 #[cfg(test)]
